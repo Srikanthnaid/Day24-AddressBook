35dd7cdcee0cc5219530281102273fd0b480d64b
Srikanthnaid <ksru044gmail.com>
2022-12-13 04:45:22 +0530

    Added

---
+++ lib/src/main/java/Day24/AddressBookSystem/AddressBook.java
@@ -0,0 +1,10 @@
+package Day24.AddressBookSystem;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("--------------Welcome to addressBook System : ---------------");
+
+	}
+
+}
